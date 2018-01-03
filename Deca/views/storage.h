06cc18f487836de0667f119cd2f5b86fbb1ba008
stt920 <931040676@qq.com>
2018-01-03 16:17:02 +0800

    add 入库管理‘‘新增’功能实现

--- Deca/views/storage.h
+++ Deca/views/storage.h
@@ -2,7 +2,7 @@
 #define STORAGE_H
 
 #include <QWidget>
-
+//入库管理
 namespace Ui {
 class storage;
 }
@@ -14,6 +14,13 @@ class storage : public QWidget
 public:
     explicit storage(QWidget *parent = 0);
     ~storage();
+
+private slots:
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
 
 private:
     Ui::storage *ui;
